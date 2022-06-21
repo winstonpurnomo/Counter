b5ee19d12c4301404d5e21a983d8ea1eb778cb66
Winston Purnomo <winstonpurnomo@gmail.com>
2022-06-21 22:20:32 +0700

    Initial version

--- Sources/Counter/Counter.swift
+++ Sources/Counter/Counter.swift
@@ -1,6 +1,36 @@
-public struct Counter {
-    public private(set) var text = "Hello, World!"
+//
+//  Counter.swift
+//  Pylike
+//
+//  Created by Winston Purnomo on 6/16/22.
+//
 
-    public init() {
+import Foundation
+
+public struct Counter<T: Hashable>: CustomStringConvertible {
+    public var description: String {
+        return dict.description
+    }
+    
+    var dict: [T: Int] = [:]
+    
+    init(_ val: String) where T == String {
+        for char in val {
+            self.dict[String(char), default: 0] += 1
+        }
+    }
+    
+    init(_ val: Array<T>) {
+        for elem in val {
+            self.dict[elem, default: 0] += 1
+        }
+    }
+    
+    func elements() -> [T] {
+        return Array(dict.keys)
+    }
+    
+    mutating func clear() {
+        dict = [:]
     }
 }
